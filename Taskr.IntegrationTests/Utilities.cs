2469d1d1c40a99c16830a920778d0327b76bfc12
DBankx <61208493+DBankx@users.noreply.github.com>
2020-12-22 22:26:04 +0000

    changed to in memory testing for the db

---
+++ Taskr.IntegrationTests/Utilities.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Taskr.Domain;
+using Taskr.Persistance;
+
+namespace Taskr.IntegrationTests
+{
+    public static class Utilities
+    {
+        public static void InitializeDbForTests(DataContext db)
+        {
+            db.Jobs.AddRange(GetSeedingMessages());
+            db.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(DataContext db)
+        {
+            db.Jobs.RemoveRange(db.Jobs);
+            InitializeDbForTests(db);
+        }
+
+        public static List<Job> GetSeedingMessages()
+        {
+            return new List<Job>()
+            {
+                new Job()
+                {
+                    Description = "test job 1 desc",
+                    Title = "test job 1",
+                    InitialPrice = 200
+                },
+                new Job()
+                {
+                    Description = "test job 2 desc",
+                    Title = "test job 2",
+                    InitialPrice = 300
+                }
+            };
+        } 
+    }
+}
--- Taskr.IntegrationTests/WebApiTestFactory.cs
+++ Taskr.IntegrationTests/WebApiTestFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 using Taskr.Api;
 using Taskr.Persistance;
 using WebMotions.Fake.Authentication.JwtBearer;
@@ -26,6 +28,7 @@ namespace Taskr.IntegrationTests
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseContentRoot(".");
+            
             builder
                 .UseTestServer()
                 .ConfigureTestServices(collection =>
@@ -36,14 +39,41 @@ namespace Taskr.IntegrationTests
                         options.DefaultChallengeScheme = FakeJwtBearerDefaults.AuthenticationScheme;
                     }).AddFakeJwtBearer();
                 });
-            // builder.ConfigureServices(services =>
-            // {
-            //     services.RemoveAll(typeof(DataContext));
-            //     services.AddDbContext<DataContext>(options =>
-            //     {
-            //         options.UseInMemoryDatabase("TestDb");
-            //     });
-            // });
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<DataContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<DataContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<DataContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<WebApplicationFactory<Startup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the " +
+                                            "database with test messages. Error: {Message}", ex.Message);
+                    }
+                }
+            }); 
             base.ConfigureWebHost(builder);
         }
         
